247b724e310481520dd7f95ac8259aef7979aa6e
TurjoSiam <t.siam24@gmail.com>
2024-12-04 19:16:50 +0600

    data send to database added

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k0g53.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        const database = client.db("MovieSphere").collection("allMovies")
+
+
+
+        app.post('/allmovies', async(req, res) => {
+            const newMovie = req.body;
+            console.log(newMovie);
+            const result = await database.insertOne(newMovie);
+            res.send(result);
+        })
+
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    catch (error) {
+        console.log('ERROR:', error);
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.get("/", (req, res) => {
     res.send('server is running');
