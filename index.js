1dfd84faacbfa4ecbd0c10d22820c82fc304a4f7
TurjoSiam <t.siam24@gmail.com>
2024-12-07 16:51:13 +0600

    featured and dynamic movie functionality added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,9 +29,26 @@ async function run() {
 
         const database = client.db("MovieSphere").collection("allMovies")
 
+        app.get('/allmovies', async (req, res) => {
+            const cursor = database.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
+        app.get('/featuredmovies', async (req, res) => {
+            const cursor = database.find().sort({ rating: -1 }).limit(6);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-        app.post('/allmovies', async(req, res) => {
+        app.get('/allmovies/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await database.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/allmovies', async (req, res) => {
             const newMovie = req.body;
             console.log(newMovie);
             const result = await database.insertOne(newMovie);
